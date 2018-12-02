5380b4d31208762e72c3079dc240ecad7750b2e0
Mr_Nicknacker <romik_slav@mail.ru>
2018-12-02 14:37:07 +0300

    Урок 6. Список. Разобрали пару методов класса List. Создал свои списки точек и символов.

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,14 +10,63 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            /*Хочу при создании класса точки сразу задавать её вид и коортинады*/
+            int px = 0;
+            int py = 0;
+
             Point p1 = new Point(1,3,'*');
             p1.Draw();
-            /*Инкапсуляция - данные и методы объеденены в класс и детали реализации
-             * скрыты от пользователя этого класса*/
+            
             Point p2 = new Point(4,5,'#');
             p2.Draw();
-            
+
+            /*-----------------LIST------------------------*/
+            List<int> numList = new List<int>();
+            numList.Add(0);
+            numList.Add(1);
+            numList.Add(2);
+
+            int x = numList[0]; /*В x передаём значение первого элемента списка*/
+            int y = numList[1]; /*В y передаём значение второго элемента списка*/
+            int z = numList[2];
+
+            foreach(int i in numList)
+            {
+                Console.WriteLine("\n"+i);
+            }
+
+            numList.RemoveAt(0); /*Удалить элемент с порядковым номером 0 из списка*/
+
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
+                        
+            /*Домашняя работа*/
+            /*Инициализирую список символов*/
+            List<char> symList = new List<char>();
+            symList.Add('*');
+            symList.Add('#');
+            symList.Add('$');
+            symList.Add('%');
+
+            Console.Clear();
+
+            /*Инициализирую список точек*/
+            Console.WriteLine("Сколько точек нужно создать?");
+            int n = Convert.ToInt32( Console.ReadLine());
+            List<Point> _pList = new List<Point>();
+
+            for (int i = 0; i<n; i++)
+            {
+                Point p = new Point(1+px, 3+py, symList[i]);
+                px++;py++;
+                _pList.Add(p);
+            }
+
+            foreach (Point pnt in _pList)
+            {
+                pnt.Draw();
+            }
+
             Console.ReadLine();
         }
     }
