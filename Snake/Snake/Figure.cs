e1fffda7d196e9018f45d47affd020d236a545d0
Mr_Nicknacker <romik_slav@mail.ru>
2018-12-02 15:51:20 +0300

    Создали базовый класс фигура. Классы для вертикальной и горизонтальной линии - наследники Фигуры (частный случай Фигуры)

---
+++ Snake/Snake/Figure.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    /*Наследование - св-во системы, позволяющее описать новый класс на основе уже существующего с частично или полностью замещающейся функциональностью*/
+    class Figure //Базовый класс для своих наследников
+    {
+        protected List<Point> pList; //идентификатор доступа. Даёт наследникам видеть переменную
+
+        public void Draw()
+        {
+            foreach (Point p in pList)
+            {
+                p.Draw();
+            }
+        }
+    }
+}
