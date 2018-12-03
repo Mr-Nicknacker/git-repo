2a3cbac25b3a63ca9ded0288f847bdcf15d1fd4c
Mr_Nicknacker <romik_slav@mail.ru>
2018-12-04 01:03:37 +0300

    Добавил метод Move, чтобы двигать змейку - стираем хвост, перемещаем голову.

--- Snake/Snake/Snake.cs
+++ Snake/Snake/Snake.cs
@@ -13,8 +13,11 @@ namespace Snake
 {
     class Snake : Figure
     {
-        public Snake(Point tail, int length, Direction direction)
+        Direction direction;
+
+        public Snake(Point tail, int length, Direction _direction)
         {
+            direction = _direction;
             pList = new List<Point>();
             for (int i=0; i< length; i++)
             {
@@ -22,6 +25,26 @@ namespace Snake
                 p.Move(i, direction);
                 pList.Add(p);
             }
+        }
+
+        internal void Move()
+        {
+            Point tail = pList.First(); //Выдаёт первый элемент списка
+            pList.Remove(tail);//Удаляет первое упоминание tail из списка
+            Point head = GetNextPoint();
+            pList.Add(head);
+
+            tail.Clear(); //координаты tail, а символ меняем на ' '
+            head.Draw();
+        }
+
+//Генерируем точку, следующую после головы в заданном направлении (двигаем змейку)
+        public Point GetNextPoint()
+        {
+            Point head = pList.Last();
+            Point nextPoint = new Point(head);
+            nextPoint.Move(1, direction);
+            return nextPoint;
         }
     }
 }
