da05203c3a55982444167fbb93e2f072171f61b4
Mr_Nicknacker <romik_slav@mail.ru>
2018-12-03 23:43:10 +0300

    Класс, который создаёт змейку по заданым параметрам: размер, направление, начальная точка

---
+++ Snake/Snake/Snake.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*Абстрагирование - способ выделить набор значимых характеристик объекта, изучая из рассмотрения незначимые*/
+/*Важно:
+ - Змейка - некоторая фигура на карте;
+ - состоит из точек
+ - стартовая позиция змейки на карте
+ - конкретное направление*/
+namespace Snake
+{
+    class Snake : Figure
+    {
+        public Snake(Point tail, int length, Direction direction)
+        {
+            pList = new List<Point>();
+            for (int i=0; i< length; i++)
+            {
+                Point p = new Point(tail);
+                p.Move(i, direction);
+                pList.Add(p);
+            }
+        }
+    }
+}
